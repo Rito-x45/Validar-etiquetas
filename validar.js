5ef4ad118951959049a2d5fbb61546dd29fc4385
Kuro <muges453012@gmail.com>
2025-02-24 19:58:15 -0600

    Segundo intento mi gente

--- validar.js
+++ validar.js
@@ -12,25 +12,26 @@ function validar(form) {
         return false;
     }
 
-    var sexo = form.sexo.value; //obtine el valor seleccionado
+    var sexo = form.sexo.value; //obtiene el valor seleccionado
     if (sexo === "") {
         alert("Por favor, seleccione su sexo.");
         return false;
     }
 
-    var deporte = form.deporte.value; //obtine el valor seleccionado
+    var deporte = form.deporte.value; //obtiene el valor seleccionado
     if (deporte === "ninguno") {
         alert("Por favor, seleccione un deporte favorito.");
         return false;
     }
 
     // Si todo es válido 
+    alert("Datos subidos correctamente"); // Muestra un mensaje de éxito
     return true;
 }
 
 // Función para detectar si hay etiquetas HTML en el texto
 function Verificar(texto) {
-    var revision = /<\/?[a-z][^>]*>/i; 
+    var revision = /<\/?[a-z][^>]*>/i;
     //Comprueba si el texto contiene etiquetas HTML 
     if (revision.test(texto)) { //se usa  para comprobar si una cadena de texto coincide con un patrón determinado.
         alert("Se han detectado etiquetas HTML");
